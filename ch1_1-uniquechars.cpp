2e3081e4a78784002f9bfe1935b08ce5db2f6516
niwachih <46865734+niwachih@users.noreply.github.com>
2022-02-26 22:25:13 +0800

    Update ch1_1-uniquechars.cpp
    [Description]
    fix bool array NOT initialized problem
    
    [Test]
    Can test a string if duplicate character exists

--- ch1_1-uniquechars.cpp
+++ ch1_1-uniquechars.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 bool isuniqueChars(string str)
 {
-	bool *charset = new bool[128];
+	bool *charset = new bool[128]();
+	//CHIH: create a bool array and init it to false
 	if(str.length() > 128)
 		return false;
 	for(int i=0;i<str.length();i++)
 	{
 		int val = str[i];
-		
+
 		if(charset[val])
 			return false;
-		
+
 		charset[val] = true;
 		//CHIH: 標示為已出現過!!
 	}
@@ -33,3 +34,19 @@ int main() {
 	
     return 0;
 }
+//test result:
+//D:\crackingbook>g++ -o test ch1_1-uniquechars.cpp
+
+//D:\crackingbook>test.exe
+//Please enter your string...
+//chiaowei
+//String is NOT unique...
+
+//D:\crackingbook>
+//D:\crackingbook>
+//D:\crackingbook>test.exe
+//Please enter your string...
+//jack
+//String is unique!
+
+//D:\crackingbook>
