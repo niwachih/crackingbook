3fa5fe57f975e799b3814be8bed454cda3e68bb6
niwachih <46865734+niwachih@users.noreply.github.com>
2022-02-27 22:23:48 +0800

    add file for leetcode medium problem

---
+++ ch1_e-myatoi.cpp
@@ -0,0 +1,50 @@
+//CHIH: leetcode medium problem
+//20220227
+int myAtoi(char * s){
+    char *ptr = s;
+    char *ptr_walker = NULL;
+    int ret = 0;
+    int sign;
+    int i;
+    
+    if ((*ptr <= 'z') && (*ptr >= 'a'))
+        return 0;
+    while (*ptr == ' ')
+        ptr++;
+    if ((ptr[0] == '+') && (ptr[1] == '-'))
+        return 0;
+    if ((ptr[0] == '-') && (ptr[1] == '+'))
+        return 0;
+    if (*ptr == '-') {
+        //negative number
+        sign = -1;
+        ptr++;
+    } else if (*ptr == '+') {
+        sign = 1;
+        ptr++;
+    } else {
+        sign = 1;
+    }
+    while (*ptr == '0')
+        ptr++;        
+    ptr_walker = ptr;
+    for (i = 0; ptr[i] != '\0'; i++) {
+        if ((ptr[i] == ' ') || (ptr[i] == '.'))
+            ptr[i] = '\0';
+    }
+    for (i = 0; ptr[i] != '\0'; i++) {
+        if (i >= 9) {
+            if (sign == 1)
+                return pow(2,31);
+            else
+                return pow(2,31)*(-1);
+        } else {
+            ret = (ret * 10) + (ptr[i] - '0');
+        }
+        if (ptr[i] == '-')
+            return 0;
+        if ((ptr[i] <= 'z') && (ptr[i] >= 'a'))
+            return 0;
+    }
+    return ret*sign;
+}
