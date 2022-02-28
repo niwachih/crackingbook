7a8f45e2b886c0cc68caf67b3096283a8212f52e
niwachih <46865734+niwachih@users.noreply.github.com>
2022-02-28 21:56:23 +0800

    Update ch1_f-addtwonumlist.cpp
    [Description]
    adjust size according to l1 and l2
    
    [Test]
    Can pass test cases that l1 and l2 length are different.

--- ch1_f-addtwonumlist.cpp
+++ ch1_f-addtwonumlist.cpp
@@ -6,15 +6,32 @@
  * };
  */
 
+int sizeoflist(struct ListNode* l) {
+    int ret = 0;
+    struct ListNode* ptr = l;
+    
+    while (ptr != NULL) {
+        ptr = ptr->next;
+        ret++;
+    }
+    
+    return ret;
+}
+
 struct ListNode* addTwo(struct ListNode* l1, struct ListNode* l2, int carry) {
     struct ListNode* ret;
     struct ListNode* ret_more;
-    struct ListNode* ptr;
-    int size_ret = 100;
+    int size_ret;
+    int size_l1, size_l2;
     int value = carry;
     
     if (l1 == NULL && l2 == NULL && carry == 0)
         return NULL;
+    if (l1 != NULL)
+        size_l1 = sizeoflist(l1);
+    if (l2 != NULL)
+        size_l2 = sizeoflist(l2);
+    size_ret = (size_l1 > size_l2) ? size_l1 : size_l2;
     ret = malloc(sizeof(struct ListNode)*(size_ret+1));
     if (l1 != NULL)
         value += l1->val;
