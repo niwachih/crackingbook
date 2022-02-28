b37a242a983dec5126e4e1e3053a2beaec6d8c13
niwachih <46865734+niwachih@users.noreply.github.com>
2022-02-28 21:40:47 +0800

    Update ch1_f-addtwonumlist.cpp
    [Description]
    fix runtime error and use recursive+carry to solve this question
    
    [Test]
    Can pass test cases and submit success
    https://leetcode.com/problems/add-two-numbers/submissions/

--- ch1_f-addtwonumlist.cpp
+++ ch1_f-addtwonumlist.cpp
@@ -6,39 +6,35 @@
  * };
  */
 
-
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+struct ListNode* addTwo(struct ListNode* l1, struct ListNode* l2, int carry) {
     struct ListNode* ret;
+    struct ListNode* ret_more;
     struct ListNode* ptr;
-    int size_l1 = 1;
-    int size_l2 = 1;
-    int size_ret;
-    int i;
-
- 
-    ptr = l1;
-    while (ptr->next) {
-        ptr = ptr->next;
-        size_l1++;
-    }
-    ptr = l2;
-    while (ptr->next) {
-        ptr = ptr->next;
-        size_l2++;
-    }
-    size_ret = (size_l1>size_l2) ? size_l1:size_l2;
+    int size_ret = 100;
+    int value = carry;
+    
+    if (l1 == NULL && l2 == NULL && carry == 0)
+        return NULL;
     ret = malloc(sizeof(struct ListNode)*(size_ret+1));
-    for (i=0;i<size_ret;i++) {
-        if (i>size_l1)
-            ret->val = l2->val;
-        else if (i>size_l2)
-            ret->val = l1->val;
-        else
-            ret->val = l1->val + l2->val;
-        ret = ret->next;
+    if (l1 != NULL)
+        value += l1->val;
+    if (l2 != NULL)
+        value += l2->val;
+    
+    ret->val = value % 10;
+    
+    if ((l1 != NULL) || (l2 != NULL)) {
+        ret_more = addTwo((l1 == NULL) ? NULL : l1->next,
+                          (l2 == NULL) ? NULL : l2->next,
+                          (value >= 10) ? 1 : 0);
+        ret->next = ret_more;
+    } else {
+        ret->next = NULL;
     }
-    //ret->val = l1->val + l2->val;
-    //ret->next = NULL;
     
     return ret;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    return addTwo(l1, l2, 0);
 }
